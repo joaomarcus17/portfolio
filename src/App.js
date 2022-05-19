34ce9e63b9c7283e81c75473ca341acfb1220bf7
joaomarcusADS <85314242+joaomarcusADS@users.noreply.github.com>
2022-05-19 20:31:31 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,46 +1,65 @@
-import React from 'react';
-import { NavBar, DivCont, DivPai, ImgProgrammer, DivFilho, DivLogo, DivLinks, Links, LetrasBanner, DivFundo, LetrasP} from './stylePort/style.js';
-import programmer from './images/programmer.png'
-import eu from './images/eu.png'
-
+import React from "react";
+import {
+  NavBar,
+  DivCont,
+  DivPai,
+  ImgProgrammer,
+  DivFilho,
+  DivLogo,
+  DivLinks,
+  Links,
+  LetrasBanner,
+  DivFundo,
+  LetrasP,
+  DivFotoT
+} from "./stylePort/style.js";
+import programmer from "./images/programmer.png";
+import eu from "./images/eu.png";
 
 function App() {
-    return (
-        <DivPai>
-            <DivFilho>
-                <header>
-                    <NavBar>
-                        <DivLogo>
-                            <Links href="#">Home</Links>
-                        </DivLogo>
-                        <DivLinks>
-                            <Links href="#">Sobre mim</Links>
-                            <Links href="#">Portfolio</Links>
-                            <Links href="#">Contatos</Links>
-                        </DivLinks>
-                    </NavBar>
-                </header>
-                <DivFundo>
-                    <ImgProgrammer src={programmer} />
-                    <div>
-                        <LetrasBanner>João Marcus</LetrasBanner>
-                        <LetrasP>Programador Web</LetrasP>
-                    </div>
-                </DivFundo>
-                <DivCont>
-                    <h1>Sobre mim</h1>
-                </DivCont>
-               
-        
-                    <p>Olá, sou João Marcus, tenho 22 anos e sou um estudante de Análise e Desenvolvimento de Sistemas da Faminas - Muriaé e um futuro Programador Web. Gosto de ler, ouvir musica, aprender coisas novas e de desafios.</p>
+  return (
+    <DivPai>
+      <DivFilho>
+        <header>
+          <NavBar>
+            <DivLogo>
+              <Links href="#">Home</Links>
+            </DivLogo>
+            <DivLinks>
+              <Links href="#">Sobre mim</Links>
+              <Links href="#">Portfolio</Links>
+              <Links href="#">Contatos</Links>
+            </DivLinks>
+          </NavBar>
+        </header>
+        <DivFundo>
+          <ImgProgrammer src={programmer} />
+          <div>
+            <LetrasBanner>João Marcus</LetrasBanner>
+            <LetrasP>Programador Web</LetrasP>
+          </div>
+        </DivFundo>
+        <DivCont>
+          <h1>Sobre mim</h1>
+        </DivCont>
+        <DivFotoT>
+          <div>
+            <img src={eu} />
+          </div>
+          <p>
+            Olá, sou João Marcus, tenho 22 anos e sou um estudante de Análise e
+            Desenvolvimento de Sistemas da Faminas - Muriaé e um futuro
+            Programador Web. Gosto de ler, ouvir musica, aprender coisas novas e
+            de desafios.
+          </p>
+        </DivFotoT>
 
-                
-                <section class="Portfolio">
-                    <h1>Portfolio</h1>
-                </section>
-            </DivFilho>
-        </DivPai>
-    );
+        <div>
+          <h1>Portfolio</h1>
+        </div>
+      </DivFilho>
+    </DivPai>
+  );
 }
 
 export default App;
--- src/stylePort/style.js
+++ src/stylePort/style.js
@@ -1,58 +1,70 @@
-import styled from 'styled-components'
-import banner from '../images/banner.png'
+import styled from "styled-components";
+import banner from "../images/banner.png";
 
 export const DivPai = styled.div`
-  background-color: #E5E5E5;
+  background-color: #e5e5e5;
   display: flex;
   justify-content: center;
   align-items: center;
-
-`
+`;
 export const DivFilho = styled.div`
   width: 80em;
-`
+`;
 
 export const DivLogo = styled.div`
-display: flex;
-`
+  display: flex;
+`;
 export const NavBar = styled.div`
   display: flex;
-  margin: 0.50em;
+  margin: 0.5em;
   justify-content: space-between;
-  `
+`;
 export const DivLinks = styled.div`
   display: flex;
   justify-content: space-between;
   width: 15em;
-`
+`;
 export const Links = styled.a`
   text-decoration: none;
-`
+  color: #990285;
+`;
 
 export const DivCont = styled.div`
   display: flex;
   justify-content: center;
-`
+  color: #990285;
+`;
 export const DivFundo = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  width: 100%;
+  width: auto;
   background-image: url(${banner});
+  background-size: cover;
 
-  `
+  div {
+    text-align: center;
+    padding: 0 50px;
+  }
+`;
 
-export const ImgProgrammer = styled.img`
-
-  
-  `
+export const ImgProgrammer = styled.img``;
 
 export const LetrasBanner = styled.h1`
- 
- margin-right: 8em;
-`
+  color: #c4c4c4;
+  font-size: 5em;
+  margin-top: 1em;
+`;
 
 export const LetrasP = styled.p`
-margin-left: 2em;
+  color: #c4c4c4;
+  font-size: 3em;
+  text-align: center;
+  margin-top: -1em;
+  margin-left: -0.5em;
+`;
 
-`
+export const DivFotoT = styled.div`
+  display: flex;
+  justify-content: space-around;
+`;
